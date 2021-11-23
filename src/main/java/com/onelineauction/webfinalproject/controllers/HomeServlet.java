ecde706841083b048f0f85cd97fdfbf6cc4a5cea
Sahb9 <71537975+Sahb9@users.noreply.github.com>
2021-11-23 22:50:57 +0700

    setup Khung  --Tuan

---
+++ src/main/java/com/onelineauction/webfinalproject/controllers/HomeServlet.java
@@ -0,0 +1,38 @@
+package com.onelineauction.webfinalproject.controllers;
+
+//import com.ute.ecwebapp.beans.Category;
+
+
+//import com.example.cuoiky.utils.ServletUtils;
+import com.onelineauction.webfinalproject.utils.ServletUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "HomeServlet", value = "/Home/*")
+public class HomeServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String path = request.getPathInfo();
+    if (path == null || path.equals("/")) {
+      path = "/Index";
+    }
+
+    switch (path) {
+      case "/Index":
+        ServletUtils.forward("/views/partials/layout.jsp", request, response);
+        break;
+
+    }
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+  }
+}
