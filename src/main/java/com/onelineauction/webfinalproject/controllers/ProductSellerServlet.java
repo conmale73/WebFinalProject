03c5967ef90036f10ac23d2a95a7a4622bf483a8
Vũ Đức Hậu <duchauvu0703@gmail.com>
2021-12-30 21:32:16 +0700

    fix

--- src/main/java/com/onelineauction/webfinalproject/controllers/ProductSellerServlet.java
+++ src/main/java/com/onelineauction/webfinalproject/controllers/ProductSellerServlet.java
@@ -2,6 +2,7 @@ package com.onelineauction.webfinalproject.controllers;
 
 import com.onelineauction.webfinalproject.beans.DauGia;
 import com.onelineauction.webfinalproject.beans.Product;
+import com.onelineauction.webfinalproject.beans.User;
 import com.onelineauction.webfinalproject.models.DauGiaModel;
 import com.onelineauction.webfinalproject.models.ProductModel;
 import com.onelineauction.webfinalproject.utils.ServletUtils;
@@ -11,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -28,19 +30,7 @@ public class ProductSellerServlet extends HttpServlet {
                 break;
 
             case "/Detail":
-                String auctionID = request.getParameter("id");
-                Product productAuction = ProductModel.findById(auctionID);
-                List<DauGia> list1 = DauGiaModel.findAllByID(auctionID);
-
-                if (productAuction == null || list1 == null) {
-                    ServletUtils.redirect("/Home", request, response);
-                } else {
-                    request.setAttribute("daugia", true);
-                    request.setAttribute("productAuction", productAuction);
-                    request.setAttribute("auctionList", list1);
-
-                    ServletUtils.forward("/views/vwSeller/ProductDetail.jsp", request, response);
-                }
+                loadData(request, response);
                 break;
 
             default:
@@ -52,5 +42,22 @@ public class ProductSellerServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    }
+
+    private void loadData(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String auctionID = request.getParameter("id");
+        Product productAuction = ProductModel.findById(auctionID);
+        List<DauGia> list1 = DauGiaModel.findAllByID(auctionID);
+        HttpSession session = request.getSession();
+        User curUser = (User) session.getAttribute("authUser");
+        if (productAuction == null || list1 == null) {
+            ServletUtils.redirect("/Home", request, response);
+        } else {
+            request.setAttribute("daugia", true);
+            request.setAttribute("productAuction", productAuction);
+            request.setAttribute("auctionList", list1);
+            request.setAttribute("currentUser", curUser);
+            ServletUtils.forward("/views/vwSeller/ProductDetail.jsp", request, response);
+        }
     }
 }
